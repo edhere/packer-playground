7f172bf679c8a78e98265545c4855aa08fba0c0f
edmundo <er@advect.us>
2014-09-16 14:12:25 -0700

    adding zsh provisioner

--- ami/ubuntu-parm/provisioners/zsh.sh
+++ ami/ubuntu-parm/provisioners/zsh.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo apt-get --force-yes -y update
-sudo apt-get install zsh
+sudo apt-get --force-yes -y install zsh
 wget –no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O – | sh
 chsh -s /bin/zsh
