e1ef6b7123071c09c78664307e37b65e664c6b32
edmundo <er@advect.us>
2014-09-18 20:49:43 -0700

    remove provisioners directory

--- ami/ubuntu-parm/provisioners/zsh.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo apt-get --force-yes -y update
-sudo apt-get --force-yes -y install zsh
-wget –no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O – | sh
-chsh -s /bin/zsh
