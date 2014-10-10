198ca0a8a011553ae9297941e0ab09c15e44d9fc
edmundo <er@advect.us>
2014-10-10 16:32:42 -0700

    temporarily ohmyzsh theming

--- ami/ubuntu-parm/scripts/zsh.sh
+++ ami/ubuntu-parm/scripts/zsh.sh
@@ -3,5 +3,5 @@ sudo apt-get --force-yes -y update
 sudo apt-get --force-yes -y install zsh
 curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
 sudo chsh -s /bin/zsh ubuntu
-cp /home/ubuntu/.oh-my-zsh/themes/terminalparty.zsh-theme /home/ubuntu/.zshrc
-. ~/.zshrc
+#cp /home/ubuntu/.oh-my-zsh/themes/terminalparty.zsh-theme /home/ubuntu/.zshrc
+#. ~/.zshrc
