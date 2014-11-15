8c31feed79894b90d228422169c5eac440b741e9
edmundo <er@advect.us>
2014-11-14 17:32:58 -0800

    accommodate new rvm signed release strategy

--- ami/ubuntu-parm/scripts/ruby.sh
+++ ami/ubuntu-parm/scripts/ruby.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+\curl -#LO https://rvm.io/mpapis.asc
+gpg --import mpapis.asc
 \curl -L https://get.rvm.io | bash -s stable --ruby
 echo '[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm" # Load RVM function' >> ~/.bash_profile
 source ~/.bash_profile
